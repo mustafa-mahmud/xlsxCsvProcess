cf6745766a7a386f8813c11c145fd66f4c7cd618
mustafa mahmud <mithuweb000@gmail.com>
2018-04-09 21:25:11 +0600

    bootstra/css/js/ add

---
+++ view/object/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="no-cache">
+        <meta http-equiv="Expires" content="-1">
+        <meta http-equiv="Cache-Control" content="no-cache"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+        <!--[if lte IE 9]>
+        <link href='/PATH/TO/FOLDER/css/animations-ie-fix.css' rel='stylesheet'>
+        <![endif]-->
+        <title>Xlsx Csv Process</title>
+        <!-----CSS------>
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/practise.css" />
+        <link rel="stylesheet" href="web-fonts-with-css/css/fontawesome-all.min.css" />
+        <!-----JS------>
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </head>
+    <body>
+        
+        
+        <script src="js/xlsxCsvProcess.js"></script>
+    </body>
+</html>
