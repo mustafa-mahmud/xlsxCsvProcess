3543cfd4466aa71ac02019fba894f156420d7e36
mustafa mahmud <mithuweb000@gmail.com>
2018-04-09 20:16:45 +0600

    vendor/ add

--- src/model/wordProcessClass.php
+++ src/model/wordProcessClass.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace xlsxCsv\model;
 
-/**
- * Description of wordProcessClass
- *
- * @author DELL
- */
 class wordProcessClass {
-    //put your code here
+    public $ckModel="ok all";
 }
--- view/object/object.php
+++ view/object/object.php
@@ -1,8 +1,9 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include '../../vendor/autoload.php';
+
+use xlsxCsv\model\wordProcessClass;
+
+$obj=new wordProcessClass();
+echo $obj->ckModel;
 
