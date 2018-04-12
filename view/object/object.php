f5e524fa1ffa31d6d432cc245366a07987c8f044
mustafa mahmud <mithuweb000@gmail.com>
2018-04-12 19:35:31 +0600

    04-12-18 night start

--- view/object/object.php
+++ view/object/object.php
@@ -12,32 +12,31 @@ if (isset($_POST["plan"]) && !empty($_POST["plan"])) {
 
 class MyReadFilter implements IReadFilter {
 
-    public $userColRow="";
+    public $userColRow = "";
     public $arrColRowSplit = [];
-    public $arrColRowAdd=array("firstCol"=>array(),"secondCol"=>array());
-    public $dataCol=[];
+    public $arrColRowAdd = array("firstCol" => array(), "secondCol" => array());
+    public $dataCol = [];
 
-
-    public function __construct($data="") {
-        $this->userColRow=$data;
+    public function __construct($data = "") {
+        $this->userColRow = $data;
     }
 
     public function readCell($column, $row, $worksheetName = '') {
         //all column which have data
         array_push($this->dataCol, $column);
         //split- only one column 
-        if(count($this->userColRow)==1){
-            if(strtoupper($this->userColRow[0])===$column){
-                array_push($this->arrColRowSplit, $column.$row);
+        if (count($this->userColRow) == 1) {
+            if (strtoupper($this->userColRow[0]) === $column) {
+                array_push($this->arrColRowSplit, $column . $row);
             }
         }
         //add - two columns
-        else{
-            if(strtoupper($this->userColRow[0])===$column){
-                array_push($this->arrColRowAdd["firstCol"], $column.$row);
+        else {
+            if (strtoupper($this->userColRow[0]) === $column) {
+                array_push($this->arrColRowAdd["firstCol"], $column . $row);
             }
-            if(strtoupper($this->userColRow[1])===$column){
-                array_push($this->arrColRowAdd["secondCol"], $column.$row);
+            if (strtoupper($this->userColRow[1]) === $column) {
+                array_push($this->arrColRowAdd["secondCol"], $column . $row);
             }
         }
         return TRUE;
@@ -63,9 +62,16 @@ if (isset($_POST) && !empty($_POST)) {
 
             if ($splitAdd === "split") {
                 //if usr put Column have data
-                if (count($filter->arrColRowSplit)>0) {
+                if (count($filter->arrColRowSplit) > 0) {
+                    //store all split data for using
+                    $arrAllDataSplit = array();
+                    $len = count($filter->arrColRowSplit);
+                    for ($i = 0; $i < $len; $i++) {
+                        array_push($arrAllDataSplit, $spreadsheet->getActiveSheet()->getCell($filter->arrColRowSplit[$i])->getValue());
+                    }
                     //remove empty key;
                     $arrSplitFilter = array_filter($_POST);
+                    //if user put symbol
                     if (array_key_exists("splitSecondCol", $arrSplitFilter)) {
                         //key available
                         $splitSymbol = trim($arrSplitFilter["splitSecondCol"]);
@@ -75,23 +81,27 @@ if (isset($_POST) && !empty($_POST)) {
                             return FALSE;
                         }
                         else {
-                            //here availabe split 'one symbol'.........
-                            $len= count($filter->arrColRowSplit);
-                            for($i=0;$i<$len;$i++){
-                                echo $spreadsheet->getActiveSheet()->getCell($filter->arrColRowSplit[$i])->getValue();
-                            }
+                            //here available one symbol.........
+                            $processSplit= array_map(function($value){
+                                if(strchr($value, "|")!==FALSE){
+                                    return explode("|", $value);
+                                }
+                            }, $arrAllDataSplit);
+                            $endProcessSplit= array_values(array_filter($processSplit));
+                            print_r($endProcessSplit);
                         }
                     }
+                    //if user not put any symbol then take 'space' default
                     else {
                         //here available split 'space'............
                     }
                 }
                 else {//empty column
-                    $array_unique= array_unique($filter->dataCol);
-                    $array_reduce= array_reduce($array_unique, function($v1,$v2){
-                        return $v1."-".$v2;
+                    $array_unique = array_unique($filter->dataCol);
+                    $array_reduce = array_reduce($array_unique, function($v1, $v2) {
+                        return $v1 . "-" . $v2;
                     });
-                    echo "only ".ltrim($array_reduce,"-")." column have value!symbolSplit";
+                    echo "only " . ltrim($array_reduce, "-") . " column have value!symbolSplit";
                 }
             }
             if ($splitAdd === "add") {
